347b80c250163cc98f4522c7d0250537fb2b8b25
illustrious-codes <onatayodavid105@gmail.com>
2025-01-10 07:49:16 +0100

    main

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def fibonacci(length):
         init = state - init
         print(init, end=" ")
 
-num = int(input("How long do you want to run this ? "))
-fibonacci(num)
-print("Happy New Year Famz")
+# num = int(input("How long do you want to run this ? "))
+# fibonacci(num)
+# print("Happy New Year Famz")
