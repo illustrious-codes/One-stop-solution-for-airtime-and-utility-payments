4de0df3270593b0ac1c874cf6fc038740e29e06e
illustrious-codes <onatayodavid105@gmail.com>
2025-01-13 07:18:37 +0100

    main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("What ever you do consistency with your life became your habit")
+print(20 % 6)
+number = 44
+number2 = str(number)
+print(type(number2))
+print(f"I am David, in am %{number2} years old")
